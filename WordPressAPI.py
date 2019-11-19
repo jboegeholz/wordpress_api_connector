c2a445efe59a93a1475df1aaacda5ea799d0fccb
jobo8505 <>
2019-11-19 17:19:38 +0100

    rework requirements

--- WordPressAPI.py
+++ WordPressAPI.py
@@ -4,21 +4,26 @@ from typing import List
 
 
 class WordPressAPI(object):
-    def __init__(self, wp_url: str, results_per_page: int=20):
+    def __init__(self, wp_url: str, results_per_page: int = 20):
         self.wp_api_url = wp_url + "/wp-json/wp/v2"
         self.results_per_page = results_per_page
 
-    def get_posts(self)-> List:
+    def get_posts(self) -> List:
         api_function = "/posts"
         number_of_elements = self.get_number_of_posts()
         return self.get_elements(api_function, number_of_elements)
 
-    def get_number_of_posts(self)-> int:
+    def get_number_of_posts(self) -> int:
         r = requests.get(self.wp_api_url + "/posts")
         return int(r.headers["X-WP-Total"])
 
     def get_number_of_categories(self) -> int:
         r = requests.get(self.wp_api_url + "/categories")
+        return int(r.headers["X-WP-Total"])
+
+    def get_number_of_posts_per_category(self, category):
+        api_function = "/posts?categories=" + str(category)
+        r = requests.get(self.wp_api_url + api_function)
         return int(r.headers["X-WP-Total"])
 
     def get_categories(self) -> List:
--- main.py
+++ main.py
@@ -3,22 +3,17 @@ from csv_export import write_csv
 from WordPressAPI import WordPressAPI
 WP_BLOG_URL = "https://creatronix.de"
 
-DATE_FORMAT = '%d/%m/%Y %H:%M'
-
-
-
-
-def create_schedule(start_date):
-    # csv format
-    # tt/mm/jjjj hh:mm, message, url
-    print(start_date.strftime(DATE_FORMAT))
-    write_csv("schedule", ["tt/mm/jjjj hh:mm", "message", "url"])
 
 
 if __name__ == '__main__':
     wp_api = WordPressAPI(WP_BLOG_URL)
     number_of_posts = wp_api.get_number_of_posts()
-    #posts = wp_api.get_posts()
+    print("Number of posts: " + str(number_of_posts))
     categories = wp_api.get_categories()
-    print(categories)
-    #create_schedule(datetime.datetime.now())
+    print("Number of categories: " + str(len(categories)))
+    category_names = []
+    for cat in categories:
+        category_names.append((cat['id'], cat['name']))
+    print("Number of posts per category: ")
+    for cat in category_names:
+        print(cat[1] + " " + str(wp_api.get_number_of_posts_per_category(cat[0])))
